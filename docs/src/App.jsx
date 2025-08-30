140f90cc78ed26f3eeb9b9f6809b7ecb6d1da19d
SAHILKUMARSAKSHI <sahilkumarsakshi_ec22a18_12@dtu.ac.in>
2025-08-30 11:44:10 +0530

    new files

--- docs/src/App.jsx
+++ docs/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom'
 import Navbar from './component/Navbar'
+import Footer from './component/Footer'
 import Dashboard from './pages/Dashboard'
 import MembersInfo from './pages/MembersInfo'
 import TrainerInfo from './pages/TrainerInfo'
@@ -11,9 +12,9 @@ import Feedback from './pages/Feedback'
 const App = () => {
   return (
     <Router>
-      <div className="min-h-screen bg-gray-900">
+      <div className="min-h-screen bg-gray-900 flex flex-col">
         <Navbar />
-        <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
+        <main className="flex-1 max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8 w-full">
           <Routes>
             <Route path="/" element={<Navigate to="/dashboard" replace />} />
             <Route path="/dashboard" element={<Dashboard />} />
@@ -24,6 +25,7 @@ const App = () => {
             <Route path="/feedback" element={<Feedback />} />
           </Routes>
         </main>
+        <Footer />
       </div>
     </Router>
   )
